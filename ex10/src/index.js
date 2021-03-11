ce2df4b9ad5b2935285f69ec159d442f0631e897
mehmedsaric <mehmed.saaric@gmail.com>
2021-03-11 22:07:46 +0100

    provjeren zadatak

---
+++ ex10/src/index.js
@@ -0,0 +1,10 @@
+function main(){ 
+var myFirstName = "Mehmed";
+var myLastName = "Saric";
+var myHomeTown = "Sarajevo";
+
+return {myFirstName, myLastName, myHomeTown };
+
+}
+console.log(main());
+module.exports = main;
